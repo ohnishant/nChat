4a30f405f0b5d4e8362b71b01db9046f44f5ca89
OhNishant <nishant.mishra550@gmail.com>
2023-07-12 23:58:41 +0530

    add: username color and text message color

--- src/app.js
+++ src/app.js
@@ -1,27 +1,47 @@
 const client = new tmi.Client({
-    channels: [ 'towdan' ]
+    channels: [ 'ml7Support' ]
 })
 
 client.connect();
 
+const max_messages = 100
 
-client.on("message", (channel, tags, message, self) => {
+client.on("chat", (channel, tags, message, self) => {
 
-    const chat_message = `${tags["display-name"]}: ${message}`;
-    // console.log(chat_message);
-    // console.log(tags)
+    // const chat_message = `${tags["display-name"]}: ${message}`;
+    const username = document.createTextNode(`${tags["display-name"]}: `);
+    const user_message = document.createTextNode(`${message}`);
 
-    var tag = document.createElement('div');
-    tag.id = tags["id"];
+    console.log(tags)
 
-    var text = document.createTextNode(chat_message);
-    tag.appendChild(text);
+    var msg = document.createElement('div');
+    msg.id = tags["id"];
+    msg.className = "message";
 
+    // username shenanigans
+    msg_user = document.createElement('span');
+    msg_user.appendChild(username)
+    msg_user.style.color = tags["color"];
+
+    // message content shenanigans
+    msg_content = document.createElement("span");
+    msg_content.class = "message-content"
+    msg_content.appendChild(user_message);
+
+    msg.appendChild(msg_user);
+    msg.appendChild(msg_content);
+    
     var chatbox = document.getElementById("message-box");
+    
+    const message_count = chatbox.children.length;
+    if (message_count >= max_messages) {
+        // chatbox.remove(chatbox.firstElementChild);
+    }
 
-    chatbox.appendChild(tag)
+    chatbox.appendChild(msg)
 
     // console.log(chat_message);
     // console.log(message_element);
+    chatbox.scrollTop = chatbox.scrollHeight;
 
 })
